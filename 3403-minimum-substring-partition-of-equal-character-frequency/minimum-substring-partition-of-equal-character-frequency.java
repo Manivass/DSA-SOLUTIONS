0e5312bcec199fd9f692ce2ef88b2b1fa3856c4e
Manivass <manivassv@gmail.com>
2026-09-12 07:28:01 +0530

    Time: 161 ms (44.91%) | Memory: 47.2 MB (14.41%) - LeetSync

--- 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.java
+++ 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.java
@@ -1,56 +1,40 @@
 class Solution {
+    HashMap<Integer , Integer> dp = new HashMap<>() ;
+    public boolean isBalanced( int[] freq ) {
+        int count = 0 ;
+        for( int x : freq ) {
+            if( x == 0 ) continue ;
 
-    HashMap<Integer, Integer> dp = new HashMap<>();
+            if( count == 0 ) {
+                count = x ;
+            }
 
-    public boolean isBalanced(int[] freq) {
-
-        int count = 0;
-
-        for (int x : freq) {
-
-            if (x == 0)
-                continue;
-
-            if (count == 0)
-                count = x;
-
-            else if (count != x)
-                return false;
+            if( count != x ) return false ;
         }
-
-        return true;
+        return true ;
     }
+    public int backtrack( String s , int st ) {
+        if( s.length() == st ) return 0 ;
 
-    public int backtrack(String s, int st) {
+        int[] freq = new int[26] ;
+        if( dp.containsKey( st ) ) return dp.get(st) ;
+        int min = Integer.MAX_VALUE ;
 
-        if (st == s.length())
-            return 0;
 
-        if (dp.containsKey(st))
-            return dp.get(st);
+        for( int i = st ; i < s.length() ; i++ ) {
+            freq[ s.charAt( i ) - 'a' ]++ ;
 
-        int min = Integer.MAX_VALUE;
-
-        int[] freq = new int[26];
-
-        for (int i = st; i < s.length(); i++) {
-
-            freq[s.charAt(i) - 'a']++;
-
-            if (isBalanced(freq)) {
-
-                int res = backtrack(s, i + 1);
-
-                min = Math.min(min, 1 + res);
+            if( isBalanced( freq ) ) {
+                int res = backtrack( s , i + 1 ) ;
+                min = Math.min( min , 1 + res ) ;
             }
         }
 
-        dp.put(st, min);
+        dp.put( st , min ) ;
 
-        return min;
+        return min ;
     }
-
     public int minimumSubstringsInPartition(String s) {
-        return backtrack(s, 0);
+        return backtrack( s , 0 ) ;
     }
 }
