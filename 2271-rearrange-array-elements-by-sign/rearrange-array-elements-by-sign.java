0c5df522367a1d7f86cd8f9254a21bf50cb40798
Manivass <manivassv@gmail.com>
2026-09-24 19:52:07 +0530

    Time: 11 ms (6.53%) | Memory: 173.1 MB (88.33%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,37 +1,47 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer> positive = new ArrayList<>() ;
-        List<Integer> negative = new ArrayList<>() ;
-        for( int num : nums ) {
-            if( num < 0 ) negative.add( num ) ;
-            else positive.add( num ) ;
+
+        List<Integer> positive = new ArrayList<>();
+        List<Integer> negative = new ArrayList<>();
+
+        for (int num : nums) {
+            if (num < 0)
+                negative.add(num);
+            else
+                positive.add(num);
         }
-        int i = 0 ;
-        int j = 0 ;
-        int k =0 ;
-        int[] arr = new int[ nums.length ] ;
-        while( i != positive.size() && j != negative.size() ) {
-            if( k == 0 || k % 2 == 0 ) {
-                arr[k] = positive.get( i ) ;
-                i++ ;
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        int[] arr = new int[nums.length];
+
+        while (i != positive.size() && j != negative.size()) {
+
+            if (k % 2 == 0) {
+                arr[k] = positive.get(i);
+                i++;
+            } else {
+                arr[k] = negative.get(j);
+                j++;
             }
-            else {
-                arr[k] = negative.get( j ) ;
-                j++ ;
-            }
-            k++ ;
+
+            k++;
         }
-        System.out.println(k) ;
-        while( j == negative.size() && k  != nums.length ) {
-            arr[k] = positive.get(j) ;
-            j++ ;
-            k++ ;
+
+        while (i < positive.size()) {
+            arr[k] = positive.get(i);
+            i++;
+            k++;
         }
-        while( i == positive.size() && k != nums.length) {
-            arr[k] = negative.get(j) ;
-            i++ ;
-            k++ ;
+
+        while (j < negative.size()) {
+            arr[k] = negative.get(j);
+            j++;
+            k++;
         }
-        return arr ;
+
+        return arr;
     }
 }
