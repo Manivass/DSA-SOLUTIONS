648d92f4a29f4f1d2c25685d8e3ce2751be79d72
Manivass <manivassv@gmail.com>
2026-09-25 07:40:08 +0530

    Time: 1 ms (95.94%) | Memory: 47.3 MB (95.03%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,24 +1,35 @@
 class Solution {
     public void setZeroes(int[][] arr) {
+
         int m = arr.length;
         int n = arr[0].length;
-        Queue<int[]> q = new ArrayDeque<>();
+
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (arr[i][j] == 0)
-                    q.offer(new int[] { i, j });
+
+                if (arr[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
             }
         }
 
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int row = curr[0];
-            int col = curr[1];
-            for (int j = 0; j < m; j++) {
-                arr[j][col] = 0;
+        for (int i = 0; i < m; i++) {
+            if (rows[i]) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = 0;
+                }
             }
-            for (int j = 0; j < n; j++) {
-                arr[row][j] = 0;
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (cols[j]) {
+                for (int i = 0; i < m; i++) {
+                    arr[i][j] = 0;
+                }
             }
         }
     }
